c4cfc4756d2751d25605b49833ad47bae4663052
jvrza <jvrza20@gmail.com>
2023-12-11 18:28:18 -0600

    Update script.js
    Testing different redirect_uri

--- src/script.js
+++ src/script.js
@@ -19,7 +19,8 @@ export async function redirectToAuthCodeFlow(clientId) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    //params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", "https://javreza.github.io/polirithm/callback"
     params.append("scope", "user-read-private user-read-email");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
