23c408548e41306173700fd7bcc24cf6fd10bb99
jvrza <jvrza20@gmail.com>
2023-12-13 13:12:01 -0600

    trying fetch

--- script.js
+++ script.js
@@ -2,20 +2,21 @@ import "./node_modules/express/lib/express.js";
 
 var client_id = '6e45be20d64d40be8c2c5b51da9dec00';
 var redirect_uri = 'http://localhost:8888/callback';
-
-var app = express();
-
-app.get('/login', function(req, res) {
-
-  var state = generateRandomString(16);
-  var scope = 'user-read-private user-read-email';
-
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
+var uri = 'https://accounts.spotify.com/authorize?' +
+querystring.stringify({
+  response_type: 'code',
+  client_id: client_id,
+  scope: scope,
+  redirect_uri: redirect_uri,
+  state: state
 });
+
+fetch(uri).then(function (response){
+
+  console.log('Successful', response);
+
+}).catch(function (err){
+
+  console.warn("Error", err)
+
+})
